f6a3296bccbb8046cccbc70658ede2d07a565c1b
MadinaQalandarova <madinaqalandarova22@gmail.com>
2026-09-20 00:25:41 +0500

    bitdii

--- exercise/main.py
+++ exercise/main.py
@@ -1,43 +1,43 @@
-#Topshiriq 1
+#Topshiriq 1 salomlashish
 
 ism = input()
 print(f"Salom, {ism}!")
 
-#Topshiriq 2
+#Topshiriq 2 yilini hisoblash
 
 age = int(input("Tug'ilgan yilingizni kiriting: "))
 print(f"Siz {2026-age} yoshdasz!")
 
-#Topshiriq 3
+#Topshiriq 3 perimetrni hisoblash
 
 p = float(input("Kvadratning tomonini kiriting: "))
 print(f"Kvadratning perimetri: {4 * p}")
 
-#Topshiriq 4
+#Topshiriq 4 qoshilgan mevalar ro'yxatini joriy ro'yxatga chiqarish
 
 fruits = input("Mevalarni kiriting: ").split(" ")
 print(f"Mevalar: {', '.join(fruits)}")
 
 
-#Topshiriq 5
+#Topshiriq 5 son kubi va kvadratini hisoblash
 
 number = int(input("Sevimli soningni yoz: "))
 print(f"Sevimli soningni kvadrati: {number ** 2}")
 print(f"Sevimli soningni kubi: {number ** 3}")
 
-#Topshiriq 6
+#Topshiriq 6 raqam indeksini chiqarish
 
 sonlar = [10, 20, 30, 40, 50]
 print(sonlar[0])
 print(sonlar[-1])
 
-#Topshiriq 7
+#Topshiriq 7 nomi kiritilgan mevalar ro'yxatiga yangi meva qo'shish
 
 mevalar = ["olma", "banan", "apelsin", "nok"]
 mevalar.insert(1, "uzum")
 print(mevalar)
 
-#Topshiriq 8
+#Topshiriq 8 ro'yxatga yangi ro'yxat qo'shish
 
 
 cars = []
@@ -45,24 +45,24 @@ mashina = input("Mashinalar nomini kiritng: ")
 cars.append(mashina)
 print(cars)
 
-#Topshiriq 9
+#Topshiriq 9 olib tashlash
 
 regions = ["Toshkent", "Samarqand", "Buxoro", "Farg'ona"]
 print(regions.remove("Buxoro"))
 
-#Topshiriq 10
+#Topshiriq 10 tartiblangan ro'yxatni chiqarish
 
 raqamlar = [41, 27, 3, 44, 50]
 raqamlar.sort()
 print(raqamlar)
 
-#Topshiriq 11
+#Topshiriq 11 o'rta arifmetik qiymatni hisoblash
 
 a = [15, 25, 35, 45]
-b =  sum(a) / len(a)     #o'rta arifmetik qiymatni hisoblash
+b =  sum(a) / len(a)     
 print(b)
 
-#Topshiriq 12   
+#Topshiriq 12   max va min qiymatni chiqarish
 
 big = input("Sonlarni kiriting: ").split(" ")
 c = max(big)
