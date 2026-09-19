9c68eaa8cc434d42aeccb4eb1e1487874cd91dde
MadinaQalandarova <madinaqalandarova22@gmail.com>
2026-09-19 23:10:57 +0500

    vazifa

--- exercise/main.py
+++ exercise/main.py
@@ -1,4 +1,4 @@
-"""#Topshiriq 1
+#Topshiriq 1
 
 ism = input()
 print(f"Salom, {ism}!")
@@ -11,22 +11,28 @@ print(f"Siz {2026-age} yoshdasz!")
 #Topshiriq 3
 
 p = float(input("Kvadratning tomonini kiriting: "))
-print(f"Kvadratning perimetri: {4 * p}")"""
+print(f"Kvadratning perimetri: {4 * p}")
 
 #Topshiriq 4
-"""
+
 fruits = input("Mevalarni kiriting: ").split(" ")
-print(f"Mevalar: {', '.join(fruits)}")"""
+print(f"Mevalar: {', '.join(fruits)}")
 
 
 #Topshiriq 5
-"""
+
 number = int(input("Sevimli soningni yoz: "))
 print(f"Sevimli soningni kvadrati: {number ** 2}")
-print(f"Sevimli soningni kubi: {number ** 3}")"""
+print(f"Sevimli soningni kubi: {number ** 3}")
 
 #Topshiriq 6
 
 sonlar = [10, 20, 30, 40, 50]
 print(sonlar[0])
 print(sonlar[-1])
+
+#Topshiriq 7
+
+mevalar = ["olma", "banan", "apelsin", "nok"]
+mevalar.insert(1, "uzum")
+print(mevalar)
