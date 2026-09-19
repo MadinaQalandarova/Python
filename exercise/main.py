2897d683428c433824840c10dedd6b41f485881c
MadinaQalandarova <madinaqalandarova22@gmail.com>
2026-09-19 22:05:53 +0500

    exercises

---
+++ exercise/main.py
@@ -0,0 +1,14 @@
+#Topshiriq 1
+
+ism = input()
+print(f"Salom, {ism}!")
+
+#Topshiriq 2
+
+age = int(input("Tug'ilgan yilingizni kiriting: "))
+print(f"Siz {2026-age} yoshdasz!")
+
+#Topshiriq 3
+
+p = float(input("Kvadratning tomonini kiriting: "))
+print(f"Kvadratning perimetri: {4 * p}")
