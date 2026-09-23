bd7ad41f38690f11f3aa5a7ffb246fdd7bd32022
MadinaQalandarova <madinaqalandarova22@gmail.com>
2026-09-23 23:33:39 +0500

    topshiriq2

--- exercise/main.py
+++ exercise/main.py
@@ -92,7 +92,7 @@ if rangi in ranglar:
     print(f"Bu rang indeks raqami: {ranglar.index(rangi)}")   
 else:
     print("Bunday rang mavjud emas!")
-
+"""
 #topshiriq tuple
 mevalar = ("olma", "banan", "olma", "anor", "olma")
 
@@ -105,7 +105,7 @@ print(f"Banan indeksi: {banan_indeksi}")
 colors =("pushti", "yashil", "moviy", "sariq", "moviy")
 
 colorIndex = colors.index("moviy")
-print(f"Moviy rangning indeksi: {colorIndex}")"""
+print(f"Moviy rangning indeksi: {colorIndex}")
 
 # Topshiriq mustaqil. Sonning kvadrati va ildizini hisoblash
 
